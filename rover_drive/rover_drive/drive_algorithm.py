5e6526571b0f75919fede20e0585865616b67e50
= <=>
2023-11-15 05:05:26 +0000

    Drive Algorithm

---
+++ rover_drive/rover_drive/drive_algorithm.py
@@ -0,0 +1,82 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float32MultiArray
+from sensor_msgs.msg import JointState
+from sensor_msgs.msg import Joy
+from rclpy.qos import QoSProfile
+import time
+import numpy as np
+from rclpy.time import Time
+import sys
+
+class DriveControl(Node):
+
+# """
+# Intialize
+# Subscribers 
+# 1.Throttle Value 
+# 2.Steering Value 
+# 3.Trigger value 
+# 4.GUI E-Stop Commnad 
+# 5.Encoder data 
+
+# Publisher:
+# Wheel speed Values   
+
+
+
+# """
+   
+    def __init__(self):
+
+        self.pub_throttle = self.create_subscription(msg_type=)
+
+        pass
+
+    def drive_control_main(self):
+        pass
+   
+    def getThrottle(self, joyData):
+        pass
+    
+    def getSteering(self, joyData):
+        pass
+    
+    def getTriggerValue(self, joyData):
+        pass
+
+    def calculateWheelSpeed(self, throttle, steering):
+        pass
+
+    def rotateInPlace(self,trigger):
+        pass
+
+    def applyWheelSpeeds(self,wheelSpeeds):
+        pass
+
+    def checkSafety():
+        pass
+
+    def applyWheelSpeeds(self,wheelSpeeds): 
+
+        pass
+
+    def checkSafety(self):
+
+        pass
+
+    def emergencyStop(self):
+
+        pass
+
+    def monitorWheelEncoder(self):
+        
+        pass
+
+
+
+
+
+    
+    
+
