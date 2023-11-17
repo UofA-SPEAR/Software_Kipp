202981b1e85cec0bb4ffd88675bdbcfe7b177dd2
= <=>
2023-11-17 19:39:15 +0000

    Drive Algorithm Commented

--- rover_drive/rover_drive/drive_algorithm.py
+++ rover_drive/rover_drive/drive_algorithm.py
@@ -10,73 +10,71 @@ from rclpy.time import Time
 import sys
 
 class DriveControl(Node):
-
-# """
-# Intialize
-# Subscribers 
-# 1.Throttle Value 
-# 2.Steering Value 
-# 3.Trigger value 
-# 4.GUI E-Stop Commnad 
-# 5.Encoder data 
-
-# Publisher:
-# Wheel speed Values   
-
-
-
-# """
    
-    def __init__(self):
+    """
+    The DriveControl class is responsible for handling the drive control logic of a rover. It subscribes to various
+    inputs like throttle, steering, and trigger values and publishes wheel speed values based on these inputs.
+    It also includes functions for emergency stop and monitoring wheel encoders.
+    """
 
-        self.pub_throttle = self.create_subscription(msg_type=)
+    def __init__(self):
+        super().__init__('drive_control')
+        
+        
+        
 
         pass
 
     def drive_control_main(self):
+        # Main method for drive control. This method should contain the logic to continuously read input data,
+        # process it, and send commands to the rover's wheels.
         pass
    
     def getThrottle(self, joyData):
+        # Extracts the throttle value from joystick data.
+        # :param joyData: Data received from the joystick.
+        # :return: Throttle value (range: 0-1).
         pass
     
     def getSteering(self, joyData):
+        # Extracts the steering value from joystick data.
+        # :param joyData: Data received from the joystick.
+        # :return: Steering value (range: -1 to 1).
         pass
     
     def getTriggerValue(self, joyData):
+        # Extracts the trigger value from joystick data.
+        # :param joyData: Data received from the joystick.
+        # :return: Trigger value.
         pass
 
     def calculateWheelSpeed(self, throttle, steering):
+        # Calculates the wheel speeds based on throttle and steering inputs.
+        # :param throttle: Throttle input (range: 0-1).
+        # :param steering: Steering input (range: -1 to 1).
+        # :return: Array or list of wheel speeds.
         pass
 
     def rotateInPlace(self,trigger):
+        # Rotates the rover in place based on the trigger value.
+        # :param trigger: Trigger value for rotation.
         pass
 
     def applyWheelSpeeds(self,wheelSpeeds):
+        # Applies the calculated wheel speeds to the rover.
+        # :param wheelSpeeds: Array or list of wheel speeds to be applied.
         pass
 
     def checkSafety():
-        pass
-
-    def applyWheelSpeeds(self,wheelSpeeds): 
-
-        pass
-
-    def checkSafety(self):
-
+        # Checks for any safety constraints or conditions that need to be met.
+        # This function is critical for ensuring safe operation of the rover.
         pass
 
     def emergencyStop(self):
-
+        # Method to immediately stop the rover in case of an emergency.
         pass
 
     def monitorWheelEncoder(self):
-        
+        # Monitors the wheel encoders to track the movement and position of the rover.
+        # This method is important for navigation and ensuring accurate movement.
         pass
-
-
-
-
-
-    
-    
-
