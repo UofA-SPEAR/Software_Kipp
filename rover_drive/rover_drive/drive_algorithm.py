e07cc31756ecc69c1fa9421053425e8c1ac194e7
= <=>
2023-11-18 13:42:00 +0000

    Drive alogrithm to be completed

--- rover_drive/rover_drive/drive_algorithm.py
+++ rover_drive/rover_drive/drive_algorithm.py
@@ -4,77 +4,82 @@ from std_msgs.msg import Float32MultiArray
 from sensor_msgs.msg import JointState
 from sensor_msgs.msg import Joy
 from rclpy.qos import QoSProfile
+from rclpy.qos import qos_profile_sensor_data
 import time
 import numpy as np
 from rclpy.time import Time
 import sys
+from interfaces.msg import RoverCommand
+
+roverCommandTopic = "rover_controls"
+steering_upper_limit = 0
+steering_lower_limit = 0
 
 class DriveControl(Node):
    
-    """
-    The DriveControl class is responsible for handling the drive control logic of a rover. It subscribes to various
-    inputs like throttle, steering, and trigger values and publishes wheel speed values based on these inputs.
-    It also includes functions for emergency stop and monitoring wheel encoders.
-    """
-
+    
     def __init__(self):
         super().__init__('drive_control')
-        
-        
-        
 
-        pass
+        rover_Command  = self.create_subscription(msg_type=RoverCommand, 
+                                                      topic=roverCommandTopic, 
+                                                      callback=self.rover_Command_Msg, 
+                                                      qos_profile=qos_profile_sensor_data)
+        
+        # Initialize member variables
+        self.throttle = 0
+        self.steering = 0
+        self.turn = 0
+        # Additional variables for wheel speeds, etc.
+        self.wheel_speeds = [0] * 6  # Assuming 6 wheels
+                
 
-    def drive_control_main(self):
-        # Main method for drive control. This method should contain the logic to continuously read input data,
-        # process it, and send commands to the rover's wheels.
-        pass
+    def rover_Command_Msg (self, msg):
+
+        self.throttle = msg.throtle 
+        self.steering = self.steering_conversion(msg.steering)
+        self.turn = msg.turn
+        
+    def steering_conversion(value):
+
+        if value < -1 or value > 1:
+            raise ValueError("Steering value must be between -1 and 1")
+
+    # Linear interpolation between min and max radii
+        return steering_lower_limit + (steering_upper_limit - steering_lower_limit) * (value + 1) / 2
    
-    def getThrottle(self, joyData):
-        # Extracts the throttle value from joystick data.
-        # :param joyData: Data received from the joystick.
-        # :return: Throttle value (range: 0-1).
-        pass
-    
-    def getSteering(self, joyData):
-        # Extracts the steering value from joystick data.
-        # :param joyData: Data received from the joystick.
-        # :return: Steering value (range: -1 to 1).
-        pass
-    
-    def getTriggerValue(self, joyData):
-        # Extracts the trigger value from joystick data.
-        # :param joyData: Data received from the joystick.
-        # :return: Trigger value.
-        pass
 
+    #Calculate the wheel speed based on the throttle, steering data.
+    #You can add additional functions to help build this function up.
+    #Note create a global variable for the width of the base for now and any other value for deminsiosns 
     def calculateWheelSpeed(self, throttle, steering):
-        # Calculates the wheel speeds based on throttle and steering inputs.
-        # :param throttle: Throttle input (range: 0-1).
-        # :param steering: Steering input (range: -1 to 1).
-        # :return: Array or list of wheel speeds.
-        pass
+        
 
+
+        pass
+    
+    #Function for rotating in place.
+    #Ensure wheels have been fully rotated 
     def rotateInPlace(self,trigger):
-        # Rotates the rover in place based on the trigger value.
-        # :param trigger: Trigger value for rotation.
+        
         pass
-
-    def applyWheelSpeeds(self,wheelSpeeds):
-        # Applies the calculated wheel speeds to the rover.
-        # :param wheelSpeeds: Array or list of wheel speeds to be applied.
+    
+    #Completed later on 
+    def applyWheelSpeeds(self,wheelSpeeds, encoderWheels):
+        
         pass
-
+    
+    #Completed later on 
     def checkSafety():
-        # Checks for any safety constraints or conditions that need to be met.
-        # This function is critical for ensuring safe operation of the rover.
+        
         pass
-
+    
+    #Completed later on 
     def emergencyStop(self):
-        # Method to immediately stop the rover in case of an emergency.
+        
         pass
-
+    
+    #Completed later on 
     def monitorWheelEncoder(self):
-        # Monitors the wheel encoders to track the movement and position of the rover.
-        # This method is important for navigation and ensuring accurate movement.
+        
         pass
