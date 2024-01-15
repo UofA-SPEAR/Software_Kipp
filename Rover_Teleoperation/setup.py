a43b6fc8b4f3d38dedfc4b7f563dd4b387d7fb24
= <=>
2024-01-15 23:24:22 +0000

    Update - Unkown

--- Rover_Teleoperation/setup.py
+++ Rover_Teleoperation/setup.py
@@ -10,6 +10,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['launch/joystick.launch.py'])
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
