bcfed1d69cf952af15a0439b89f6ec1741a0604e
Induwara <kandapah@ualberta.ca>
2024-02-20 22:47:45 -0700

    Git fix

--- src/kipp_camera/setup.py
+++ src/kipp_camera/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'kipp_camera'
 
@@ -10,12 +12,15 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='smart',
     maintainer_email='kandapah@ualberta.ca',
-    description='TODO: Package description',
+    description='Launches the zed camera nodes for the kipp rover ',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
--- src/kipp_control/setup.py
+++ src/kipp_control/setup.py
@@ -1,8 +1,6 @@
 from setuptools import find_packages, setup
-from glob import glob
-import os
 
-package_name = 'kipp_camera'
+package_name = 'kipp_control'
 
 setup(
     name=package_name,
@@ -12,15 +10,12 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
-        
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='smart',
     maintainer_email='kandapah@ualberta.ca',
-    description='Launches the zed camera nodes for the kipp rover ',
+    description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
