a0c0967a75d3da15a2cc255bb97d261a5fef777b
Induwara <kandapah@ualberta.ca>
2024-02-21 18:53:34 -0700

    Camera

--- src/kipp_camera/launch/kipp_camera.launch.py
+++ src/kipp_camera/launch/kipp_camera.launch.py
@@ -12,17 +12,29 @@ def generate_launch_description():
     )
 
     # Define the ZED wrapper node with your custom parameters
-    zed_wrapper_node = Node(
+    zed_wrapper_node_main = Node(
         package='zed_wrapper',  # Replace with your package name if different
         executable='zed_wrapper',  # Replace with the correct executable name if different
         name='zed_node',  # You can rename your node if needed
-        namespace='zed',  # Adjust the namespace as required
+        namespace='zed_main',  # Adjust the namespace as required
         parameters=[config_path],  # Path to your custom config file
         output='screen',
-        remappings=[('odom', 'kipp_camera_odom')]
+        remappings=[('odom', 'kipp_camera_main_odom')]
     )
+
+    zed_wrapper_node_back = Node(
+        package='zed_wrapper',  # Replace with your package name if different
+        executable='zed_wrapper',  # Replace with the correct executable name if different
+        name='zed_node',  # You can rename your node if needed
+        namespace='zed_back',  # Adjust the namespace as required
+        parameters=[config_path],  # Path to your custom config file
+        output='screen',
+        remappings=[('odom', 'kipp_camera_map_odom')]
+    )
+
 
     # Return the LaunchDescription object
     return LaunchDescription([
-        zed_wrapper_node
+        zed_wrapper_node_main,
+        zed_wrapper_node_back
     ])
