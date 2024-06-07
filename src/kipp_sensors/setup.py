c2ecfc9ec53a4ef70f241741c042be1f1595ed88
Ayden <aydenbravender@gmail.com>
2024-06-06 20:12:14 -0600

    kipp_sensor package

--- src/kipp_sensors/setup.py
+++ src/kipp_sensors/setup.py
@@ -1,8 +1,6 @@
 from setuptools import find_packages, setup
-from glob import glob
-import os
 
-package_name = 'kipp_nav'
+package_name = 'kipp_sensors'
 
 setup(
     name=package_name,
@@ -12,13 +10,11 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='smart',
-    maintainer_email='kandapah@ualberta.ca',
+    maintainer='ayden',
+    maintainer_email='ayden@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
