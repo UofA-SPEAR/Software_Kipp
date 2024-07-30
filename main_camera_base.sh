e5cca414ae3e8e16bdbe48bc9a022b11a88a0d84
Induwara <kandapah@ualberta.ca>
2024-07-29 21:02:44 -0600

    Updated Pipline G streamer

--- main_camera_base.sh
+++ main_camera_base.sh
@@ -1,3 +1,10 @@
-gst-launch-1.0 -v udpsrc port=5000 caps="application/x-rtp, media=video, encoding-name=H265, payload=96" ! \
- rtph265depay ! h265parse ! avdec_h265 ! tee name=t ! queue ! autovideosink t. ! queue ! \
- autovideoconvert ! tcpserversink host=0.0.0.0 port=7000
+#!/bin/bash
+
+while true; do
+  gst-launch-1.0 udpsrc port=5000 ! application/x-rtp,clock-rate=90000,payload=96 ! \
+  queue ! rtph264depay ! h264parse ! avdec_h264 ! \
+  queue ! autovideoconvert ! fpsdisplaysink
+
+  echo "Receiving pipeline exited. Restarting in 5 seconds..."
+  sleep 5
+done
--- main_camera_rover.sh
+++ main_camera_rover.sh
@@ -1,5 +1,13 @@
-gst-launch-1.0 -v zedsrc camera-sn=31826195 ! timeoverlay ! tee name=split has-chain=true ! \
- queue ! autovideoconvert ! fpsdisplaysink \
- split. ! queue max-size-time=0 max-size-bytes=0 max-size-buffers=0 ! autovideoconvert ! \
- nvh265enc bitrate=3000 ! h265parse ! rtph265pay config-interval=-1 pt=96 ! queue ! \
- udpsink host=192.168.1.2 port=5000 max-bitrate=3000000 sync=false async=false
+#!/bin/bash
+
+while true; do
+  gst-launch-1.0 zedsrc ! timeoverlay ! tee name=split has-chain=true ! \
+  queue ! autovideoconvert ! fpsdisplaysink \
+  split. ! queue max-size-time=0 max-size-bytes=0 max-size-buffers=0 ! autovideoconvert ! \
+  x264enc byte-stream=true tune=zerolatency speed-preset=ultrafast bitrate=3000 ! \
+  h264parse ! rtph264pay config-interval=-1 pt=96 ! queue ! \
+  udpsink clients=192.168.1.169:5000 max-bitrate=3000000 sync=false async=false
+
+  echo "Pipeline exited. Restarting in 5 seconds..."
+  sleep 5
+done
