6f5d82caaeb6b8f1ec778571df77868bf25661f3
Induwara <kandapah@ualberta.ca>
2024-07-29 21:04:44 -0600

    Ip Address updated

--- main_camera_rover.sh
+++ main_camera_rover.sh
@@ -6,7 +6,7 @@ while true; do
   split. ! queue max-size-time=0 max-size-bytes=0 max-size-buffers=0 ! autovideoconvert ! \
   x264enc byte-stream=true tune=zerolatency speed-preset=ultrafast bitrate=3000 ! \
   h264parse ! rtph264pay config-interval=-1 pt=96 ! queue ! \
-  udpsink clients=192.168.1.169:5000 max-bitrate=3000000 sync=false async=false
+  udpsink clients=192.168.1.10:5000 max-bitrate=3000000 sync=false async=false
 
   echo "Pipeline exited. Restarting in 5 seconds..."
   sleep 5
