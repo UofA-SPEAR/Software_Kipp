4a4e2297bef1dc8b1825bbc2d1d74f9cf4417dc4
Induwara <kandapah@ualberta.ca>
2024-07-29 21:12:10 -0600

    Second Camera added

---
+++ backup_camera_base.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+while true; do
+  gst-launch-1.0 udpsrc port=5002 ! application/x-rtp,clock-rate=90000,payload=96 ! \
+  queue ! rtph264depay ! h264parse ! avdec_h264 ! \
+  queue ! autovideoconvert ! fpsdisplaysink
+
+  echo "Receiving pipeline exited. Restarting in 5 seconds..."
+  sleep 5
+done
---
+++ backup_camera_rover.sh
@@ -0,0 +1,11 @@
+while true; do
+  gst-launch-1.0 zedsrc camera-sn=camera-sn=15549043 ! timeoverlay ! tee name=split has-chain=true ! \
+  queue ! autovideoconvert ! fpsdisplaysink \
+  split. ! queue max-size-time=0 max-size-bytes=0 max-size-buffers=0 ! autovideoconvert ! \
+  x264enc byte-stream=true tune=zerolatency speed-preset=ultrafast bitrate=3000 ! \
+  h264parse ! rtph264pay config-interval=-1 pt=96 ! queue ! \
+  udpsink clients=192.168.1.10:5002 max-bitrate=3000000 sync=false async=false
+
+  echo "Pipeline exited. Restarting in 5 seconds..."
+  sleep 5
+done
