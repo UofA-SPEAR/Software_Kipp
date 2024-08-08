e64f507fa45093836a706620f6f15e9a52e3457e
Induwara <kandapah@ualberta.ca>
2024-08-08 15:33:16 -0600

    infrard camera updated

--- infrared_camera_base.sh
+++ infrared_camera_base.sh
@@ -2,9 +2,11 @@
 
 while true; do
   gst-launch-1.0 udpsrc port=5010 ! application/x-rtp,clock-rate=90000,payload=96 ! \
-  queue ! rtph264depay ! h264parse ! avdec_h264 ! \
-  queue ! autovideoconvert ! fpsdisplaysink
+  queue ! rtph264depay ! h264parse ! tee name=t ! queue ! \
+  avdec_h264 ! videoconvert ! autovideosink \
+  t. ! queue ! matroskamux ! filesink location=output.mkv sync=false async=false
 
   echo "Receiving pipeline exited. Restarting in 5 seconds..."
   sleep 5
 done
+
