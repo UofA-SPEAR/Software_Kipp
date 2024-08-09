1478b947d7585af9a46d0ac324df28c6a0cd65a3
Induwara <kandapah@ualberta.ca>
2024-08-08 18:58:15 -0600

    updated pipeline

--- main_camera_base.sh
+++ main_camera_base.sh
@@ -1,11 +1,10 @@
-# #!/bin/bash
+#!/bin/bash
 
-# while true; do
-#   gst-launch-1.0 udpsrc port=5000 ! application/x-rtp,clock-rate=90000,payload=96 ! \
-#   queue ! rtph264depay ! h264parse ! avdec_h264 ! \
-#   queue ! autovideoconvert ! fpsdisplaysink
+while true; do
+  gst-launch-1.0 udpsrc port=5000 ! application/x-rtp,clock-rate=90000,payload=96 ! \
+  queue ! rtph264depay ! h264parse ! avdec_h264 ! \
+  queue ! autovideoconvert ! fpsdisplaysink
 
-#   echo "Receiving pipeline exited. Restarting in 5 seconds..."
-#   sleep 5
-# done
-gst-launch-1.0 udpsrc port=1234 ! application/x-rtp, payload=127 ! rtph264depay ! avdec_h264 ! xvimagesink sync=false
+  echo "Receiving pipeline exited. Restarting in 5 seconds..."
+  sleep 5
+done
