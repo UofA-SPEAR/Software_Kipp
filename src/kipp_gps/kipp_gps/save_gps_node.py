ad601cb2e90d666c66fcb79c9553263b7fbf6e34
Induwara <kandapah@ualberta.ca>
2024-08-09 22:50:51 -0600

    Save GPS NODE UPDATE

--- src/kipp_gps/kipp_gps/save_gps_node.py
+++ src/kipp_gps/kipp_gps/save_gps_node.py
@@ -1,20 +1,16 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import NavSatFix
-from std_srvs.srv import SetBool
-import subprocess
 
 class SaveGpsNode(Node):
 
     def __init__(self):
         super().__init__('save_gps_node')
-        self.srv = self.create_service(SetBool, 'trigger_action', self.trigger_action_callback)
         self.subscription = self.create_subscription(NavSatFix, 'gps/fix', self.gps_callback, 10)
         self.gps_data = None
-        self.saving_enabled = False
-        self.timer = None
-        self.file_path = 'gps_coordinates/gps_coordinates.txt'  # File to save GPS coordinates
-        self.get_logger().info('Service node has been started.')
+        self.file_path = 'src/kipp_gps/kipp_gps/gps_coordinates.txt'  # File to save GPS coordinates
+        self.timer = self.create_timer(2.0, self.save_gps_callback)  # Save every 2 minutes (120 seconds)
+        self.get_logger().info('GPS saving node has been started.')
 
     def gps_callback(self, msg):
         self.gps_data = msg
@@ -39,53 +35,6 @@ class SaveGpsNode(Node):
             self.get_logger().info('GPS position written to file.')
         except Exception as e:
             self.get_logger().error(f'Failed to write to file: {e}')
-
-        # file_path = "/home/ayden/Software_Kipp/src/kipp_sensors/kipp_sensors/gps_coordinates.txt" # change
-        # server_ip = "10.0.0.171" # change as needed
-        # port = 12345
-
-        # # command = f"ros2 run kipp_file_transfer file_send --ros-args -p file_path:={file_path} -p server_ip:={server_ip} -p port:={port}"
-
-        # try:
-        #     result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        #     print("Output:")
-        #     print(result.stdout)
-        #     if result.stderr:
-        #         print("Errors:")
-        #         print(result.stderr)
-        # except subprocess.CalledProcessError as e:
-        #     print(f"Command '{command}' failed with return code {e.returncode}.")
-        #     print("Error output:")
-        #     print(e.stderr)
-
-
-    def trigger_action_callback(self, request, response):
-        if request.data:
-            if not self.saving_enabled:
-                self.saving_enabled = True
-                self.timer = self.create_timer(2.0, self.save_gps_callback)
-                self.get_logger().info('GPS saving enabled.')
-                response.success = True
-                response.message = 'GPS position saving enabled.'
-            else:
-                self.get_logger().info('GPS saving already enabled.')
-                response.success = True
-                response.message = 'GPS position saving was already enabled.'
-        else:
-            if self.saving_enabled:
-                self.saving_enabled = False
-                if self.timer is not None:
-                    self.timer.cancel()
-                    self.timer = None
-                self.get_logger().info('GPS saving disabled.')
-                response.success = True
-                response.message = 'GPS position saving disabled.'
-            else:
-                self.get_logger().info('GPS saving already disabled.')
-                response.success = True
-                response.message = 'GPS position saving was already disabled.'
-
-        return response
 
 def main(args=None):
     rclpy.init(args=args)
