075e35d33e224fda99e528f35a54a97252bcfaad
Ayden <aydenbravender@gmail.com>
2024-08-10 16:35:06 -0600

    updated rover_arm

--- Startup Scripts/rover_arm.sh
+++ Startup Scripts/rover_arm.sh
@@ -23,5 +23,9 @@ tmux send-keys -t $SESSION_NAME:3 "/home/spearua/Desktop/Software_Kipp/main_came
 tmux new-window -t $SESSION_NAME:4 -n "ArmCamera"
 tmux send-keys -t $SESSION_NAME:4 "/home/spearua/Desktop/Software_Kipp/arm_camera_rover.sh" C-m
 
+#Aruco Camera
+tmux new-window -t $SESSION_NAME:5 -n "ArucoCamera"
+tmux send-keys -t $SESSION_NAME:5 "source /home/spearua/Desktop/Software_Kipp/install/setup.bash; source /home/spearua/Desktop/kipp_circ_arm/install/setup.bash; ros2 run kipp_camera aruco_node" C-m
+
 # Optional: Attach to the tmux session to view the windows
 tmux attach-session -t $SESSION_NAME
