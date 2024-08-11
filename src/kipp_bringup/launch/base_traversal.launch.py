19f47f2199be163e649d45f0c19b3156db8d21d4
Ayden <aydenbravender@gmail.com>
2024-08-11 10:41:06 -0600

    launch

--- src/kipp_bringup/launch/base_traversal.launch.py
+++ src/kipp_bringup/launch/base_traversal.launch.py
@@ -7,7 +7,8 @@ from launch_ros.actions import Node, ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
 
 def generate_launch_description():
-
+    
+    #--------------------Drive Nodes ---------------------------
 
     joy_node = Node(
             package='joy',
@@ -18,11 +19,34 @@ def generate_launch_description():
             parameters=[{'autorepeat_rate': 20.0}, {'deadzone': 0.1}],
             namespace='manual',
         )
+
+    #--------------------GPS Node------------------------------
+
+    gps_record = Node(
+            package='kipp_gps',
+            executable='gps_record',
+            name='gps_record',
+            output='screen',
+            emulate_tty=True,
+        )
     
+    #--------------------ARM Node--------------------------------
+
+    arm_node = Node(
+        package='kipp_arm_encoder',
+        executable='kipp_arm_calibrate',
+        name='kipp_arm_calibrate',
+        output='screen',
+    )
+
+
 
     return launch.LaunchDescription([
         SetEnvironmentVariable(name='RCUTILS_COLORIZED_OUTPUT', value='1'),
-        joy_node
+        #gps_record,
+        joy_node,
+        # arm_node
+
     ])
 
 if __name__ == '__main__':
